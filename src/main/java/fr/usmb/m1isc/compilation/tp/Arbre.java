b8440116046b3aceadc4b8d0b5e1e6ef5846c092
RemiChavance <remi.chavance@orange.fr>
2023-03-30 16:14:57 +0200

    ex 1

--- src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
@@ -1,5 +1,8 @@
 package fr.usmb.m1isc.compilation.tp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.util.Objects.isNull;
 
 public class Arbre {
@@ -7,6 +10,9 @@ public class Arbre {
     private TypeNode type;
     private String racine;
     private Arbre fg, fd;
+
+    private final String TABULATION = "\t";
+    private final String RETOUR_LIGNE = "\n";
 
     public Arbre(TypeNode type, String racine, Arbre fg, Arbre fd) {
         this.type = type;
@@ -43,6 +49,83 @@ public class Arbre {
         if ((this.type == TypeNode.OPERATEUR) || (this.type == TypeNode.LET)) {
             resultat += ")";
         }
+        return resultat;
+    }
+
+
+    public String generateAllCode() {
+        StringBuilder code = new StringBuilder();
+        code.append("DATA SEGMENT" + RETOUR_LIGNE);
+
+        List<String> listData = new ArrayList<>();
+        this.generateDataSection(listData);
+
+        for (String data : listData) {
+            code.append(TABULATION);
+            code.append(data);
+            code.append(" DD" + RETOUR_LIGNE);
+        }
+
+        code.append("DATA ENDS" + RETOUR_LIGNE);
+        code.append("CODE SEGMENT" + RETOUR_LIGNE);
+        code.append(this.generateCodeSection());
+        code.append("CODE ENDS");
+        return code.toString();
+    }
+
+    private void generateDataSection(List<String> data) {
+        if (!isNull(this.fg)) {
+            this.fg.generateDataSection(data);
+        }
+        if (!isNull(this.fd)) {
+            this.fd.generateDataSection(data);
+        }
+        if ((this.type == TypeNode.IDENT) && (!data.contains(this.racine))) {
+            data.add(this.racine);
+        }
+    }
+
+    private String generateCodeSection() {
+        String resultat = "";
+
+        // POINT VIRGULE
+        if (this.type == TypeNode.SEMI) {
+            return this.fg.generateCodeSection() + this.fd.generateCodeSection();
+        }
+
+        // ENTIER / IDENT
+        else if ((this.type == TypeNode.ENTIER) || (this.type == TypeNode.IDENT)) {
+            return TABULATION + "mov eax, " + this.racine + RETOUR_LIGNE;
+        }
+
+        // OPERATEUR (+, -, *, /)
+        else if (this.type == TypeNode.OPERATEUR) {
+            resultat += this.fg.generateCodeSection();
+            resultat += TABULATION + "push eax" + RETOUR_LIGNE;
+            resultat += this.fd.generateCodeSection();
+            resultat += TABULATION + "pop ebx" + RETOUR_LIGNE;
+            switch (this.racine) {
+                case "+" -> resultat += TABULATION + "add eax, ebx" + RETOUR_LIGNE;
+                case "*" -> resultat += TABULATION + "mul eax, ebx" + RETOUR_LIGNE;
+                case "-" -> {
+                    resultat += TABULATION + "sub ebx, eax" + RETOUR_LIGNE;
+                    resultat += TABULATION + "mov eax, ebx" + RETOUR_LIGNE;
+                }
+                case "/" -> {
+                    resultat += TABULATION + "div ebx, eax" + RETOUR_LIGNE;
+                    resultat += TABULATION + "mov eax, ebx" + RETOUR_LIGNE;
+                }
+            }
+            return resultat;
+        }
+
+        // LET
+        else if (this.type == TypeNode.LET) {
+            resultat += this.fd.generateCodeSection();
+            resultat += TABULATION + "mov " + this.fg.racine + ", eax" + RETOUR_LIGNE;
+            return resultat;
+        }
+
         return resultat;
     }
 }
--- src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
@@ -3,6 +3,8 @@ package fr.usmb.m1isc.compilation.tp;
 import java_cup.runtime.Symbol;
 
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
@@ -14,10 +16,24 @@ public class Main {
 		else
 			yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
 		@SuppressWarnings("deprecation")
+
 		parser p = new parser (yy);
 		Symbol s = p.parse( );
-		Arbre t = (Arbre) s.value;
-		System.out.println(t.toString());
+
+		Arbre arbre = (Arbre) s.value;
+		System.out.println(arbre.toString());
+
+		String code = arbre.generateAllCode();
+		System.out.println(code);
+
+		try {
+			FileWriter myWriter = new FileWriter("TP_INFO805_Lambada.asm");
+			myWriter.write(code);
+			myWriter.close();
+		} catch (IOException e) {
+			System.out.println("Could not write into file");
+			e.printStackTrace();
+		}
     }
 
 }
